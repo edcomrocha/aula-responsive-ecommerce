197fb3bbd6b705c0901327d8aabc980f35335af8
edcomrocha <edcomrocha@gmail.com>
2022-10-21 22:39:09 -0300

    Edit Pages

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Header } from "../../components/Header";
-import { HeroBanner } from "../../components/HeroBanner";
+
 import { MenuMobile } from "../../components/MenuMobile";
-import { OtherModels } from "../../components/OtherModels";
+
 import { HomeContainer } from "../../styles/pages/HomeStyles";
 
 export function Home() {
@@ -16,8 +16,8 @@ export function Home() {
       />
       <HomeContainer>
         <Header setMenuIsVisible={setMenuIsVisible}/>
-        <HeroBanner />
-        <OtherModels />
+        
+        
       </HomeContainer>
     </>
   )
